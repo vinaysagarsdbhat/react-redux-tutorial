4b5490b0f7f38d28987b5259e7bb4597eb22c99b
vinaysagarsdbhat <65327592+vinaysagarsdbhat@users.noreply.github.com>
2021-04-12 18:28:20 +0530

    Delete Part 2/react-redux-tutorial directory

--- Part 2/react-redux-tutorial/src/store.js
+++
@@ -1,6 +0,0 @@
-import { createStore } from "redux";
-import rotateReducer from "reducers/rotateReducer";
-function configureStore(state = { rotating: true }) {
-  return createStore(rotateReducer,state);
-}
-export default configureStore;
